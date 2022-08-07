773cdba128ebf90468a006bca4fe3e784b6d3f5a
Biliyoung Aishino <biliyounga@gmail.com>
2022-08-07 10:49:07 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,86 +1,32 @@
-#include <stdarg.h>
-#include "mymain.h"
-#include <stddef.h>
+#include "main.h"
 
 /**
- * get_op - select function for conversion char
- * @c: char to check
- * Return: pointer to function
+ * _printf - produces output according to a format
+ * @format: a character string
+ *
+ * Return: the number of characters printed
  */
-
-int (*get_op(const char c))(va_list)
-{
-int i = 0;
-
-flags_p fp[] = {
-{"c", print_char},
-{"s", print_str},
-{"i", print_nbr},
-{"d", print_nbr},
-{"b", print_binary},
-{"o", print_octal},
-{"x", print_hexa_lower},
-{"X", print_hexa_upper},
-{"u", print_unsigned},
-{"S", print_str_unprintable},
-{"r", print_str_reverse},
-{"p", print_ptr},
-{"R", print_rot13},
-{"%", print_percent}
-};
-while (i < 14)
-{
-if (c == fp[i].c[0])
-{
-return (fp[i].f);
-}
-i++;
-}
-return (NULL);
-}
-
-/**
- * _printf - Reproduce behavior of printf function
- * @format: format string
- * Return: value of printed chars
- */
-
 int _printf(const char *format, ...)
 {
-va_list ap;
-int sum = 0, i = 0;
-int (*func)();
+pt_fmt pt_format[] = {
+{"c", print_char},
+{"s", print_string},
+{"d", print_digit},
+{"i", print_digit},
+{"%", print_percent},
+{"b", print_binary_conv},
+{"u", print_unsig_int},
+{"o", print_oct},
+{"x", print_hex_low},
+{"X", print_hex_upper},
+{NULL, NULL}
+};
 
-if (!format || (format[0] == % &&format[1] == 0))
-return (-1);
-va_start(ap, format);
+va_list valist;
+int num_ch = 0;
 
-while (format[i])
-{
-if (format[i] == %)
-{
-if (format[i + 1] != 0)
-func = get_op(format[i + 1]);
-if (func == NULL)
-{
-_putchar(format[i]);
-sum++;
-i++;
-}
-else
-{
-sum += func(ap);
-i += 2;
-continue;
-}
-}
-else
-{
-_putchar(format[i]);
-sum++;
-i++;
-}
-}
-va_end(ap);
-return (sum);
+va_start(valist, format);
+num_ch = get_print(format, valist, pt_format);
+va_end(valist);
+return (num_ch);
 }
