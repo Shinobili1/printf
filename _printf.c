3cbd9478843cd90ab207c2fd8a0b4c8f0683c934
Amubieya <107006879+Amubieya@users.noreply.github.com>
2022-08-10 04:23:47 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,64 +1,11 @@
-
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
-
-void print_buffer(char buffer[], int *buff_ind);
 
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
-<<<<<<< HEAD
  * @format: format.
  * Return: Printed chars.
- */
-int _printf(const char *format, ...)
-{
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-
-if (format == NULL)
-return (-1);
-
-va_start(list, format);
-
-for (i = 0; format && format[i] != 0; i++)
-{
-if (format[i] != %)
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-}
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
-}
-
-print_buffer(buffer, &buff_ind);
-
-va_end(list);
-
-return (printed_chars);
-=======
- * @format: format
- * Return: Printed chars
  */
 int _printf(const char *format, ...)
 {
@@ -103,7 +50,6 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (printed_chars);
->>>>>>> 7d88b44521123f7b166bbc0ff0344b4a2d655fee
 }
 
 /**
@@ -113,15 +59,8 @@ int _printf(const char *format, ...)
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
-<<<<<<< HEAD
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-
-*buff_ind = 0;
-=======
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
->>>>>>> 7d88b44521123f7b166bbc0ff0344b4a2d655fee
 }
